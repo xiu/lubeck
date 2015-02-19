1e403aaba75c329dd61d0ab480c83cdd5aec7494
Guillaume Herail <guillaume@herail.net>
2015-02-19 14:06:05 +0100

    IT'S ALIVE! ALIIIIVE!

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # lubeck
+
+## Setup
+You'll need go setup with cross compiling
+  
+  brew install go --with-cc-all
+
+## Build
+  
+  GOARM=6 GOARCH=arm GOOS=linux go build main.go
---
+++ main.go
@@ -0,0 +1,103 @@
+package main
+
+import (
+	"fmt"
+	"os/exec"
+	"time"
+
+	"github.com/hybridgroup/gobot"
+	"github.com/hybridgroup/gobot/platforms/gpio"
+	"github.com/hybridgroup/gobot/platforms/raspi"
+)
+
+func main() {
+	gbot := gobot.NewGobot()
+
+	r := raspi.NewRaspiAdaptor("raspi")
+	ledred := gpio.NewLedDriver(r, "led-red", "3")
+	ledgreen := gpio.NewLedDriver(r, "led-green", "5")
+	ledyellow := gpio.NewLedDriver(r, "led-yellow", "7")
+	ledblue := gpio.NewLedDriver(r, "led-blue", "11")
+	ledwhite := gpio.NewLedDriver(r, "led-white", "13")
+	buttonred := gpio.NewButtonDriver(r, "button-red", "15")
+	buttongreen := gpio.NewButtonDriver(r, "button-green", "19")
+        buttonyellow := gpio.NewButtonDriver(r, "button-yellow", "21")
+        buttonblue := gpio.NewButtonDriver(r, "button-blue", "23")
+        buttonwhite := gpio.NewButtonDriver(r, "button-white", "12")
+
+	ledred.Off()
+	ledgreen.Off()
+	ledyellow.Off()
+	ledblue.Off()
+	ledwhite.Off()
+
+	ledred.On()
+	time.Sleep(100 * time.Millisecond)
+	ledgreen.On()
+	time.Sleep(100 * time.Millisecond)
+	ledyellow.On()
+	time.Sleep(100 * time.Millisecond)
+	ledblue.On()
+	time.Sleep(100 * time.Millisecond)
+	ledwhite.On()
+	time.Sleep(100 * time.Millisecond)
+
+        ledred.Off()
+        ledgreen.Off()
+        ledyellow.Off()
+        ledblue.Off()
+        ledwhite.Off()
+	
+
+	work := func() {
+		inception := exec.Command("play", "/root/inception.mp3")
+		gobot.On(buttonred.Event("push"), func(data interface{}) {
+			fmt.Println("PUSHED RED")
+			ledred.On()
+			inception.Run()
+			ledred.Off()
+		})
+
+		blabla := exec.Command("play", "/root/blabla.mp3")
+                gobot.On(buttongreen.Event("push"), func(data interface{}) {
+                        fmt.Println("PUSHED GREEN")
+                        ledgreen.On()
+                        blabla.Run()
+                        ledgreen.Off()
+                })
+
+		cotcot := exec.Command("play", "/root/cotcot.mp3")
+		gobot.On(buttonyellow.Event("push"), func(data interface{}) {
+			fmt.Println("PUSHED YELLOW")
+			ledyellow.On()
+			cotcot.Run()
+			ledyellow.Off()
+		})	
+		
+                toilets := exec.Command("play", "/root/toilets.mp3")
+                gobot.On(buttonblue.Event("push"), func(data interface{}) {
+                        fmt.Println("PUSHED BLUE")
+                        ledblue.On()
+                        toilets.Run()
+                        ledblue.Off()
+                })
+
+                whip := exec.Command("play", "/root/whip.mp3")
+                gobot.On(buttonwhite.Event("push"), func(data interface{}) {
+                        fmt.Println("PUSHED WHITE")
+                        ledwhite.On()
+                        whip.Run()
+                        ledwhite.Off()
+                })
+	}
+
+	robot := gobot.NewRobot("Lubeck",
+		[]gobot.Connection{r},
+		[]gobot.Device{buttonred, buttongreen, buttonyellow, buttonblue, buttonwhite, ledred, ledgreen, ledyellow, ledblue, ledwhite},
+		work,
+	)
+
+	gbot.AddRobot(robot)
+
+	gbot.Start()
+}
